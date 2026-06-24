19e636cf01d6bebcdab7347eb4c313b93a09e49d
Aaron Frawley <aaron.frawley01@student.wandw.ac.nz>
2026-06-24 12:45:55 +1200

    committ all

--- RecyclingWorld/obj/Debug/net8.0/RecyclingWorld.AssemblyInfo.cs
+++ RecyclingWorld/obj/Debug/net8.0/RecyclingWorld.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecyclingWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d7da8665b1361c085cccaf3aa398878c238748")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecyclingWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecyclingWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
